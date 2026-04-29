a4df2d4a7fc5cba601d26a53ec28137381913ef0
Amin A Talahmeh <ayman201266@hotmail.com>
2026-04-30 02:35:43 +0300

    Remove unused pages

--- hassil-web/src/pages/Profile.tsx
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-    return (
-        <div className="page-placeholder">
-            <h1>Profile</h1>
-            <p>This page is reserved for profile settings and user preferences.</p>
-        </div>
-    )
-}
