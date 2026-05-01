f8fb7a2a7ca9742f8d72512deac281c1b78d051d
Mahmoud <mahmoudjamal01m@gmail.com>
2026-05-01 23:57:35 +0300

    feat: update connection string for local development and adjust API proxy target

--- hassil-web/vite.config.js
+++ hassil-web/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_PROXY_TARGET ?? 'https://localhost:7087',
+        target: process.env.VITE_API_PROXY_TARGET ?? 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
